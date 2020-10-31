f68003f788f7054d1e5ad5fb16d635baf9cd640b
seanrattigan <sean.rattigan@gmail.com>
2020-10-31 23:09:36 +0000

    Added dice roll

---
+++ 01_basics/dice_roll.py
@@ -0,0 +1,12 @@
+# demo for using a module and importing one function
+
+# imports are ALWAYS at the beginning of the file
+from random import randrange
+
+print("Simulate a bunch of rolls of the dice\n")
+print randrange(1,7)
+print randrange(1,7)
+print randrange(1,7)
+print randrange(1,7)
+print randrange(1,7)
+print randrange(1,7)
