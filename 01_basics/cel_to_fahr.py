d6d074bef7a09c425715fcca1640af25245d582d
seanrattigan <sean.rattigan@gmail.com>
2020-10-31 22:28:44 +0000

    Added user input to temperature

--- 01_basics/cel_to_fahr.py
+++ 01_basics/cel_to_fahr.py
@@ -1,12 +1,13 @@
+vat_rate = 0.21
 line = "=" * 60
 print(line)
 print("\t\tTemperature Converter")
 print(line)
 
-cel = 100
+cel = input("\tEnter degrees Celsius: ")
+cel = float(cel)
 
 fahr = (9 / 5 * cel) + 32
 
-print(f"\n\t{cel} degrees C is equal to {fahr} degrees F\n")
-
+print(f"\n\t{round(cel, 2)} degrees C is equal to {round(fahr, 2)} degrees F\n")
 
