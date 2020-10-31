e86e5fe0fdf17d233b41e1709547d2bdc2a10aab
seanrattigan <sean.rattigan@gmail.com>
2020-10-31 22:26:05 +0000

    Added temperature shell

---
+++ 01_basics/cel_to_fahr.py
@@ -0,0 +1,12 @@
+line = "=" * 60
+print(line)
+print("\t\tTemperature Converter")
+print(line)
+
+cel = 100
+
+fahr = (9 / 5 * cel) + 32
+
+print(f"\n\t{cel} degrees C is equal to {fahr} degrees F\n")
+
+
