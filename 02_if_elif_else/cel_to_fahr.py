b3e060cdd9d6dc4d3fd83f0ade2b0b007f9d768a
seanrattigan <sean.rattigan@gmail.com>
2020-11-02 15:30:19 +0000

    updated cel_to_fahr

--- 02_if_elif_else/cel_to_fahr.py
+++ 02_if_elif_else/cel_to_fahr.py
@@ -1,4 +1,4 @@
-vat_rate = 0.21
+# one approach to solving this exercise
 line = "=" * 60
 print(line)
 print("\t\tTemperature Converter")
@@ -7,6 +7,13 @@ print(line)
 cel = input("\tEnter degrees Celsius: ")
 cel = float(cel)
 
+if cel < -273.15:
+    print("Absolute Zero exceeded")
+    cel = -273.15
+elif cel == -273.15:
+    print("This is absolute zero!")
+# could have used an else, but the next line executes regardless,
+# so no need.  else would have just added complexity here.
 fahr = (9 / 5 * cel) + 32
 
 print(f"\n\t{round(cel, 2)} degrees C is equal to {round(fahr, 2)} degrees F\n")
