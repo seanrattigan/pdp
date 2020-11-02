6a5b78178a9999ab2bb1c005ca3e6fbd19d79773
seanrattigan <sean.rattigan@gmail.com>
2020-11-02 15:27:03 +0000

    updated wages

--- 02_if_elif_else/wages.py
+++ 02_if_elif_else/wages.py
@@ -5,7 +5,14 @@ rate = input("Enter rate: ")
 rate = float(rate)
 hours = input("Enter hours: ")
 hours = float(hours)
-pay = rate * hours
+
+if hours <= 39:
+    pay = rate * hours
+else:
+    o_t_hrs = hours - 39
+    o_t_pay = rate * 1.5 * o_t_hrs
+    pay = 39 * rate + o_t_pay
+
 print(f"You will be paid €{pay}")
 print(line)
 
