b3b7dbbb89962a8e0e15869222ab448c5aec5343
seanrattigan <sean.rattigan@gmail.com>
2020-11-02 18:33:00 +0000

    added conditional to quadratic

--- 02_if_elif_else/quadratic.py
+++ 02_if_elif_else/quadratic.py
@@ -15,10 +15,15 @@ c = int(c)
 
 print(f"For the quadrative equation {a}x\u00b2 + {b}x + {c}")
 # ( -b +/- sqrt(b^2 - 4ac) ) / 2a
-x = b**2 - (4 * a * c)
-ans_1 = (-b + x) / 2 * a
-ans_2 = (-b - x) / 2 * a
-
-print("The roots are:")
-print(f"\t{ans_1} and")
-print(f"\t{ans_2}\n")
+y = b**2 - (4 * a * c)
+if y >= 0:
+    x = sqrt(y)
+    ans_1 = (-b + x) / 2 * a
+    ans_2 = (-b - x) / 2 * a
+    print("The roots are:")
+    print(f"\t{ans_1} and")
+    print(f"\t{ans_2}\n")
+else:
+    print("The curve does not intersect the x-axis\n")
+    # from here you can work out the imaginary roots
+    
